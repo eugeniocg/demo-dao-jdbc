300cd0d00b7a69071e3ad09761689b08c64e7395
Eugenio Cardoso <feugeniocg@gmail.com>
2020-06-06 18:56:26 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/modelo/dao/impl/FuncionarioDaoJDBC.java
@@ -0,0 +1,40 @@
+package modelo.dao.impl;
+
+import java.util.List;
+
+import modelo.dao.FuncionarioDao;
+import modelo.entidades.Funcionario;
+
+public class FuncionarioDaoJDBC implements FuncionarioDao {
+
+	@Override
+	public void inser(Funcionario objeto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Funcionario objeto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Funcionario findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Funcionario> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
