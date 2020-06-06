a2abaadd97d38db954bb950ddc176468013c8cda
Eugenio Cardoso <feugeniocg@gmail.com>
2020-06-06 17:11:51 -0300

    Department entity class

---
+++ src/Aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package Aplicacao;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento objeto = new Departamento(1, "Livros");
+		System.out.println(objeto);
+
+	}
+
+}
